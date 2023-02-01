561f0621479b33e5eeda95e247b2909b8c5be14b
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-01 23:46:33 +0000

    account.returnBalance, account.deposit, account.withdraw behave as expected

--- spec/int/int.test.js
+++ spec/int/int.test.js
@@ -8,13 +8,15 @@ describe("class Statement()", () => {
     statement.transaction(account.returnBalance());
     expect(statement.printStatement()).toEqual([1000]);
   });
+  
   it("prints an array with orig balance and balance with new total after deposit", () => {
-    
     statement.transaction(account.deposit(1000));
     expect(statement.printStatement()).toEqual([1000, 2000]);
   });
 
-  it("prints an array with the orig balance and balance with new total after withdrawal", () => {
-
+   it("prints an array with the orig balance and balance with new total after withdrawal", () => {
+    
+    statement.transaction(account.withdraw(500));
+    expect(statement.printStatement()).toEqual([1000, 2000, 1500]);
    })
 });
