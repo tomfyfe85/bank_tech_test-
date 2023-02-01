a54c4eb0e7c0dcca12d209a0428ed2fdfb2713cf
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-01 22:32:37 +0000

    changed test numbers to match readme example

--- spec/int/int.test.js
+++ spec/int/int.test.js
@@ -1,17 +1,17 @@
 const Account = require("../../lib/Account");
-const account = new Account(500);
+const account = new Account(1000);
 const Statement = require("../../lib/Statement");
 const statement = new Statement();
 
 describe("class Statement()", () => {
   it("returns printStatement", () => {
     statement.transaction(account.returnBalance());
-    expect(statement.printStatement()).toEqual([500]);
+    expect(statement.printStatement()).toEqual([1000]);
   });
   it("prints an array with orig balance and balance with new total after deposit", () => {
     
-    statement.transaction(account.deposit(500));
-    expect(statement.printStatement()).toEqual([500, 1000]);
+    statement.transaction(account.deposit(1000));
+    expect(statement.printStatement()).toEqual([1000, 2000]);
   });
 
   it("prints an array with the orig balance and balance with new total after withdrawal", () => {
