71fb165e74932084f500c9fd476f0da166a4c760
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-02 23:01:02 +0000

    'added formatting with Prettier'

--- spec/Account.test.js
+++ spec/Account.test.js
@@ -1,8 +1,8 @@
 const Account = require("../lib/Account");
-const account = new Account(1000.00);
+const account = new Account(1000.0);
 // complete tests on other functions
 describe("class Account()", () => {
   it("returns date, first deposit and balance", () => {
-    expect(account.returnBalance()).toEqual(`2/2/2023||1000||||1000`)
-  })
+    expect(account.returnBalance()).toEqual(`2/2/2023||1000||||1000`);
+  });
 });
--- spec/Statement.test.js
+++ spec/Statement.test.js
@@ -2,8 +2,7 @@ const Statement = require("../lib/Statement");
 const statement = new Statement();
 
 describe("class Statement()", () => {
-  xit("gives an empty array", () => {
-    expect(statement.printStatement ()).toEqual([]);
+  it("gives an empty array", () => {
+    expect(statement.printStatement()).toEqual([]);
   });
-
 });
