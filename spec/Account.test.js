0d111e394a37168ed49e718f4d136f0f1e407168
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-03 08:42:54 +0000

    account.deposit() passes test

--- spec/Account.test.js
+++ spec/Account.test.js
@@ -1,8 +1,12 @@
 const Account = require("../lib/Account");
-const account = new Account(1000.0);
+const account = new Account(1000.00);
 // complete tests on other functions
 describe("class Account()", () => {
   it("returns date, first deposit and balance", () => {
-    expect(account.returnBalance()).toEqual(`2/2/2023||1000||||1000`);
+    expect(account.returnBalance()).toEqual(`3/2/2023||1000||||1000`);
   });
+``
+  it("returns date, amount deposited, new balance", () => {
+    expect(account.deposit(2000.00)).toEqual(`3/2/2023||2000||||3000`)
+  })
 });
