14d0c224ceb7f35720fc6350fc047998994127e6
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-06 22:15:26 +0000

    made notes for account.deposit

--- spec/Account.test.js
+++ spec/Account.test.js
@@ -1,13 +1,13 @@
 const Account = require("../lib/Account");
-const account = new Account(1000.0000);
+const account = new Account(1000.0);
 
 // complete tests on other functions
 describe("class Account()", () => {
   it("returns date, first deposit and balance to 2 decimal places", () => {
-    expect(account.returnBalance()).toEqual(`3/2/2023||1000.00||||1000.00`);
+    expect(account.returnBalance()).toEqual(`6/2/2023||1000.00||||1000.00`);
   });
-  ``;
-  xit("returns date, amount deposited, new balance", () => {
-    expect(account.deposit(2000.0)).toEqual(`3/2/2023||2000||||3000`);
+  // incomplete -  test returns"6/2/2023||2000||||10002000" 
+  it("returns date, amount deposited, new balance", () => {
+    expect(account.deposit(2000.0)).toEqual(`6/2/2023||2000.00||||3000.00`);
   });
 });
