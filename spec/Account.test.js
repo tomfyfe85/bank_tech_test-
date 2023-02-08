de23aab9edf7eac9b0d2ea5d4d95409986c3da2b
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-08 17:46:50 +0000

    account.Return balance passes test. Note to separate concerns within Accoutn class. Note to write edge case tests Account class

--- spec/Account.test.js
+++ spec/Account.test.js
@@ -5,10 +5,11 @@ const account = new Account(1000.00
 // complete tests on other functions
 describe("class Account()", () => {
   it("returns date, first deposit and balance to 2 decimal places", () => {
-    expect(account.returnBalance()).toEqual(`7/2/2023||1000.00||||1000.00`);
+    expect(account.returnBalance()).toEqual(`8/2/2023||1000.00||||1000.00`);
   });
   // incomplete -  test returns"6/2/2023||2000||||10002000"
   it("returns date, amount deposited, new balance", () => {
-    expect(account.deposit(2000.00)).toEqual(`7/2/2023||2000.00||||3000.00`);
+    expect(account.deposit(2000.00)).toEqual(`8/2/2023||2000.00||||3000.00`);
   });
 });
+// Write more edge case tests
