a70e57913a627da39e1a5c7c3292ce9f12dbc506
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-14 00:05:39 +0000

    created account.withdraw test. Removed class Transaction idea from notes as I thought it was an unnecessary step

--- spec/Account.test.js
+++ spec/Account.test.js
@@ -5,11 +5,16 @@ const account = new Account(1000.00);
 // Write more edge case tests
 describe("class Account()", () => {
   it("returns date, first deposit and balance to 2 decimal places", () => {
-    expect(account.returnBalance()).toEqual(`13/2/2023||1000.00||||1000.00`);
+    expect(account.returnBalance()).toEqual(`14/2/2023||1000.00||||1000.00`);
   });
 
   it("returns date, amount deposited, new balance after deposit", () => {
-    expect(account.deposit(2000.0)).toEqual(`13/2/2023||2000.00||||3000.00`);
+    expect(account.deposit(2000.0)).toEqual(`14/2/2023||2000.00||||3000.00`);
   });
-});
+
+  it("returns date, amount deposited, new balance after deposit", () => {
+    expect(account.withdraw(500)).toEqual(`14/2/2023||2000.00||||2500.00`);
+  });
+}
+);
 
