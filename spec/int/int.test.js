b7f0fdf757c642ff158bbfc56cd3bf3c35c39113
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-14 12:53:44 +0000

    int test 6 for multiple transactions and statment checks - passes

--- spec/int/int.test.js
+++ spec/int/int.test.js
@@ -40,8 +40,8 @@ describe("class Statement()", () => {
       "14/2/2023 || 2000.00 |||| 3000.00",
       "14/2/2023 || 1000.00 |||| 1000.00",
     ]);
-  })
-    
+  });
+
   it("(5) returns correct output after another deposit", () => {
     statement.transaction(account.withdraw(500));
     expect(statement.printStatement()).toEqual([
@@ -52,24 +52,24 @@ describe("class Statement()", () => {
       "14/2/2023 || 2000.00 |||| 3000.00",
       "14/2/2023 || 1000.00 |||| 1000.00",
     ]);
-  })
-  
-  it("(6) returns statement twice", () => {
-    statement.statement.printStatement();
+  });
+
+  it("(6) ", () => {
+    statement.printStatement();
+    statement.transaction(account.withdraw(500));
+    statement.transaction(account.deposit(500));
+    statement.printStatement();
+    statement.transaction(account.returnBalance());
     expect(statement.printStatement()).toEqual([
       "date || credit || debit || balance",
-      "14/2/2023 |||| 500.00 || 3000.00",
-      "14/2/2023 || 1000.00 |||| 3500.00",
+      "14/2/2023 || 3000.00 |||| 3000.00",
+      "14/2/2023 || 500.00 |||| 3000.00",
       "14/2/2023 |||| 500.00 || 2500.00",
-      "14/2/2023 || 2000.00 |||| 3000.00",
-      "14/2/2023 || 1000.00 |||| 1000.00",
-      "date || credit || debit || balance",
       "14/2/2023 |||| 500.00 || 3000.00",
       "14/2/2023 || 1000.00 |||| 3500.00",
       "14/2/2023 |||| 500.00 || 2500.00",
       "14/2/2023 || 2000.00 |||| 3000.00",
       "14/2/2023 || 1000.00 |||| 1000.00",
     ]);
-  })
-
-})
+  });
+});
