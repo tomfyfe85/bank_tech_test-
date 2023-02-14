05519334c5002c734246e5dbb0b154a9b91a530b
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-14 08:26:15 +0000

    tests are now numbered. int test (3) modified to test for returning an array in reverse

--- spec/int/int.test.js
+++ spec/int/int.test.js
@@ -4,14 +4,14 @@ const Statement = require("../../lib/Statement");
 const statement = new Statement();
 
 describe("class Statement()", () => {
-  it("returns printStatement", () => {
+  it("(1)returns printStatement", () => {
     statement.transaction(account.returnBalance());
     expect(statement.printStatement()).toEqual([
       "14/2/2023||1000.00||||1000.00",
     ]);
   });
 
-  it("prints an array containing the results of account.deposit, account.balance as elements", () => {
+  it("(2)prints an array containing the results of account.deposit, account.balance as elements", () => {
     statement.transaction(account.deposit(1000));
     expect(statement.printStatement()).toEqual([
       "14/2/2023||1000.00||||2000.00",
@@ -19,12 +19,12 @@ describe("class Statement()", () => {
       ]);
   });
 
- xit("prints an array containing the results of account.balance, account.deposit and account.withdrawal as elements", () => {
+ xit("(3)prints an array containing the results of account.withdrawal, account.balance account.deposit as elements", () => {
     statement.transaction(account.withdraw(500));
     expect(statement.printStatement()).toEqual([
-      "14/2/2023||1000.00||||1000.00",
-      "14/2/2023||1000.00||||2000.00",
       "14/2/2023||||500.00||1500.00",
+      "14/2/2023||1000.00||||2000.00",
+      "14/2/2023||1000.00||||1000.00"
     ]);
   });
 });
