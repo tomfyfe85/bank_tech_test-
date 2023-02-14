781f434652dc060d78050f6118c7759d1386544e
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-14 08:44:07 +0000

    added header to int tests

--- spec/int/int.test.js
+++ spec/int/int.test.js
@@ -7,6 +7,7 @@ describe("class Statement()", () => {
   it("(1)returns printStatement", () => {
     statement.transaction(account.returnBalance());
     expect(statement.printStatement()).toEqual([
+      'date || credit || debit || balance',
       "14/2/2023||1000.00||||1000.00",
     ]);
   });
@@ -14,6 +15,7 @@ describe("class Statement()", () => {
   it("(2)prints an array containing the results of account.deposit, account.balance as elements", () => {
     statement.transaction(account.deposit(1000));
     expect(statement.printStatement()).toEqual([
+      'date || credit || debit || balance',
       "14/2/2023||1000.00||||2000.00",
       "14/2/2023||1000.00||||1000.00",
       ]);
@@ -22,6 +24,7 @@ describe("class Statement()", () => {
  xit("(3)prints an array containing the results of account.withdrawal, account.balance account.deposit as elements", () => {
     statement.transaction(account.withdraw(500));
     expect(statement.printStatement()).toEqual([
+      'date || credit || debit || balance',
       "14/2/2023||||500.00||1500.00",
       "14/2/2023||1000.00||||2000.00",
       "14/2/2023||1000.00||||1000.00"
